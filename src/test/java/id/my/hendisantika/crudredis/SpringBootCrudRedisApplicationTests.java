7dfaa54001dd75d3d253422ef901a511574fa406
hendisantika <hendisantika@yahoo.co.id>
2025-10-19 09:54:26 +0700

    feat: Add required configs🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/crudredis/SpringBootCrudRedisApplicationTests.java
+++ src/test/java/id/my/hendisantika/crudredis/SpringBootCrudRedisApplicationTests.java
@@ -3,6 +3,17 @@ package id.my.hendisantika.crudredis;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-crud-redis
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/10/25
+ * Time: 11.00
+ * To change this template use File | Settings | File Templates.
+ */
 @SpringBootTest
 class SpringBootCrudRedisApplicationTests {
 
