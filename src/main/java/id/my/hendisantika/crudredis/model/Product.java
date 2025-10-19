a6be3ffff359c8548db85076d9df1d3c5228d1a5
hendisantika <hendisantika@yahoo.co.id>
2025-10-19 09:55:19 +0700

    feat: Add Product Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/crudredis/model/Product.java
@@ -0,0 +1,54 @@
+package id.my.hendisantika.crudredis.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-crud-redis
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/10/25
+ * Time: 09.15
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * Product entity for Redis storage
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash("Product")
+public class Product implements Serializable {
+
+    @Id
+    private String id;
+
+    @NotBlank(message = "Product name is required")
+    private String name;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be positive")
+    private BigDecimal price;
+
+    @NotNull(message = "Quantity is required")
+    @Positive(message = "Quantity must be positive")
+    private Integer quantity;
+
+    private String category;
+}
