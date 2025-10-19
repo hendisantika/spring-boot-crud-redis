df0520b1c802fbd7a2f16fe1545940e9c3e6482e
hendisantika <hendisantika@yahoo.co.id>
2025-10-19 09:54:48 +0700

    feat: Add required containers🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/crudredis/SpringBootCrudRedisApplication.java
+++ src/main/java/id/my/hendisantika/crudredis/SpringBootCrudRedisApplication.java
@@ -3,6 +3,17 @@ package id.my.hendisantika.crudredis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-crud-redis
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/10/25
+ * Time: 09.00
+ * To change this template use File | Settings | File Templates.
+ */
 @SpringBootApplication
 public class SpringBootCrudRedisApplication {
 
