9dd3645e8492aa13913da567883c2eb854ea87bd
hendisantika <hendisantika@yahoo.co.id>
2025-10-19 09:24:50 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/crudredis/SpringBootCrudRedisApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.crudredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudRedisApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/crudredis/SpringBootCrudRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.crudredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
